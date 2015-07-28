841a638bac7bcfcb91eaffebcf4f0cb3c0596584
Fredy Setz <fredy.setz@gmx.ch>
2015-07-28 20:07:37 +0200

    Protocol changed from AUTO_JOIN to AUTOJOIN

--- src/main/java/ch/jass/connection/mapping/ChooseSessionDataValue.java
+++ src/main/java/ch/jass/connection/mapping/ChooseSessionDataValue.java
@@ -3,14 +3,14 @@ package ch.jass.connection.mapping;
 import ch.jass.model.SessionType;
 
 public enum ChooseSessionDataValue {
-	undefined, CREATE_NEW, AUTO_JOIN, JOIN_EXISTING;
+	undefined, CREATE_NEW, AUTOJOIN, JOIN_EXISTING;
 
 	public static final String DATA_SESSION_CHOICE = "sessionChoice";
 	public static final String DATA_SESSION_NAME = "sessionName";
 
 	public static ChooseSessionDataValue getMappedChooseSession(final SessionType sessionType) {
 		if (sessionType == SessionType.AUTO_JOIN) {
-			return AUTO_JOIN;
+			return AUTOJOIN;
 		} else if (sessionType == SessionType.CREATE_NEW) {
 			return CREATE_NEW;
 		} else if (sessionType == SessionType.JOIN_EXISTING) {
